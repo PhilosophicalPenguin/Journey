a5d7b2b4c9f67ee0a4587ba14f2290497e7e725a
Derek <derek.b26@gmail.com>
2015-05-22 15:39:16 -0700

    -basic gulp file   TASKs     -lint :         checks for syntax errors     -concatAndCompress :         if lint passes concat all js files for client and minifies that concated file, outputs to dist/all.min.js     -watch  :         watches for changes on client js files and run lint, and concatAndCompress as well for changes in css and runs minify-css     -minify-css :         minifies css files in styles directory and stores them in     -build :         runs tasks lint, concatAndCompress and minify-css     -clean :         yet to be implemented, will delete all created dist files

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var minifyCss = require('gulp-minify-css');
+
+var targetClientSourceFiles = './client/**/*.js';
+var distributeDirectory = './dist/';
+var distributeSource = distributeDirectory + '**/*.js';
+
+gulp.task('lint', function() {
+    return gulp.src([targetClientSourceFiles, '!' + distributeSource])
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('compressAndConcat', ['lint'], function() {
+    return gulp.src([targetClientSourceFiles] )
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest(distributeDirectory))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(distributeDirectory));
+});
+
+gulp.task('minify-css', function() {
+    return gulp.src('styles/*.css')
+        .pipe(minifyCss())
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest(distributeDirectory));
+});
+
+gulp.task('clean', function() {
+
+});
+
+gulp.task('watch', function() {
+    gulp.watch(targetClientSourceFiles, ['lint', 'scripts']);
+    gulp.watch('styles/*.css', ['minify-css']);
+});
+
+gulp.task('build', ['lint', 'compressAndConcat', 'minify-css']);
