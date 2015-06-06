baf7251722025c104b887017d2ae7e2c7bfe0288
Sunshine Yin <sunshine.w.yin@gmail.com>
2015-06-06 16:08:44 -0700

    Clicking on journey in navbar in journeyview takes back to homepage

--- client/views/NavBarView.js
+++ client/views/NavBarView.js
@@ -1,23 +1,21 @@
 var NavBarView = Backbone.View.extend({
   el: '#nav-bar-div-container',
-    initialize : function () {
-      this.render();
-    },
 
-    render : function() {
-      this.$el.children().detach();
-      // console.log('this el nav bar', this.$el)
+  initialize : function () {
+    this.render();
+  },
 
-        //this.$el.children().detach();
+  render : function() {
+    this.$el.children().detach();
 
-        this.$el.append('<header class="navbar navbar-static-top" role="navigation">' +
-          '<div class="container">' +
-            '<div class="navbar-header">' +
-              '<a href="#/"><h1>Journey</h1></a>' +
-            '</div>' +
-              '<form class="navbar-form"><input class="header-search" id="autocomplete" placeholder="What position are you considering?">' +
-              '</form>' +
+      this.$el.append('<header class="navbar navbar-static-top" role="navigation">' +
+        '<div class="container">' +
+          '<div class="navbar-header">' +
+            '<a href="/"><h1>Journey</h1></a>' +
           '</div>' +
-        '</header>');
-    }
+            '<form class="navbar-form"><input class="header-search" id="autocomplete" placeholder="What position are you considering?">' +
+            '</form>' +
+        '</div>' +
+      '</header>');
+  }
 });
