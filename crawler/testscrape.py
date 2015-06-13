4b80063b6012d60903823d5ea8b8791a7263bd4e
Yuriy Bash <yuriybash@gmail.com>
2015-06-13 11:52:09 -0700

    adds server tests and adds 5k profiles

--- crawler/testscrape.py
+++ crawler/testscrape.py
@@ -1,4 +1,4 @@
-# sudo scrapy runspider testscrape.py -s USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36" -o MasterJSON.json;
+# sudo scrapy runspider testscrape.py -s USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36" -o june12.json;
 
 import re
 import scrapy
@@ -27,77 +27,8 @@ class PersonScraper(CrawlSpider):
 	USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36"
 	name = 'personSpy'
 	allowed_domains = ['linkedin.com']
-	start_urls = ['https://www.linkedin.com/in/minnaking',
-  'https://www.linkedin.com/in/dsingh',
-  'https://www.linkedin.com/in/michaeljgardea',
-  'https://www.linkedin.com/in/mickdudleyjohnson',
-  'https://www.linkedin.com/in/bmauney',
-  'https://www.linkedin.com/in/seanplynch',
-  'https://www.linkedin.com/pub/rousseau-kazi/20/259/7b8',
-  'https://www.linkedin.com/in/sippey',
-  'https://www.linkedin.com/pub/ryan-singer/2b/159/994',
-  'https://www.linkedin.com/in/markchang',
-  'https://www.linkedin.com/in/aparnacd',
-  'https://www.linkedin.com/pub/garima-sinha/9/3b1/9b3',
-  'https://www.linkedin.com/in/nitinjulka',
-  'https://www.linkedin.com/in/jeannalee',
-  'https://www.linkedin.com/in/lcholmes',
-  'https://www.linkedin.com/in/elizabethburstein',
-  'https://www.linkedin.com/in/gustafalstromer',
-  'https://www.linkedin.com/in/jeffreysharris',
-  'https://www.linkedin.com/in/chelseytanaka',
-  'https://www.linkedin.com/pub/peter-birch/1/b59/568',
-  'https://www.linkedin.com/in/dshapero',
-  'https://www.linkedin.com/in/zalzally',
-  'https://www.linkedin.com/in/nicoposner',
-  'https://www.linkedin.com/in/kumareshp',
-  'https://www.linkedin.com/in/sarahtavel',
-  'https://www.linkedin.com/in/catherineshyu',
-  'https://www.linkedin.com/in/dforrester',
-  'https://www.linkedin.com/pub/mat-mullen/7/353/a02',
-  'https://www.linkedin.com/in/srothmuller',
-  'https://www.linkedin.com/in/srikanthmnaidu',
-  'https://www.linkedin.com/in/jocelynmangan',
-  'https://www.linkedin.com/in/dvdkm',
-  'https://www.linkedin.com/in/sanazahari',
-  'https://www.linkedin.com/pub/brandon-savage/3/889/231',
-  'https://www.linkedin.com/in/productmanagercalifornia',
-  'https://www.linkedin.com/in/jasontoff',
-  'https://www.linkedin.com/in/kartikrao',
-  'https://www.linkedin.com/pub/madhu-prabaker/0/3b6/9bb',
-  'https://www.linkedin.com/in/johnloof',
-  'https://www.linkedin.com/in/dweekly',
-  'https://www.linkedin.com/in/jppark',
-  'https://www.linkedin.com/in/andriheidar',
-  'https://www.linkedin.com/in/curtjohnson',
-  'https://www.linkedin.com/in/danielayele',
-  'https://www.linkedin.com/in/lizreaveswalker',
-  'https://www.linkedin.com/in/merci',
-  'https://www.linkedin.com/pub/paul-karayan/7/952/601',
-  'https://www.linkedin.com/in/maxkirsch',
-  'https://www.linkedin.com/in/salgar',
-  'https://www.linkedin.com/in/jchang1',
-  'https://www.linkedin.com/pub/matt-schnitt/55/753/941',
-  'https://www.linkedin.com/in/reevethompson',
-  'https://www.linkedin.com/in/jgolden',
-  'https://www.linkedin.com/in/maxhaltman',
-  'https://www.linkedin.com/in/roddynegron',
-  'https://www.linkedin.com/in/lennyrachitsky',
-  'https://www.linkedin.com/in/michaelboeke',
-  'https://www.linkedin.com/in/stephanieteng',
-  'https://www.linkedin.com/in/djhalliday',
-  'https://www.linkedin.com/in/deepthim',
-  'https://www.linkedin.com/pub/leili-baghaei-rad/15/735/379',
-  'https://www.linkedin.com/in/stephenchau',
-  'https://www.linkedin.com/in/stevenjlee',
-  'https://www.linkedin.com/in/ragavansrinivasan',
-  'https://www.linkedin.com/in/andrewjchang1',
-  'https://www.linkedin.com/in/iylee',
-  'https://www.linkedin.com/in/iamdeepa',
-  'https://www.linkedin.com/in/vidyachandra',
-  'https://www.linkedin.com/in/calvinclee',
-  'https://www.linkedin.com/in/stedjamulia']	
-  	rules = [Rule(SgmlLinkExtractor(allow=(r'/in/dsingh|/in/michaeljgardea|/in/mickdudleyjohnson|/in/bmauney|/in/seanplynch|/pub/rousseau-kazi/20/259/7b8|/in/sippey|/pub/ryan-singer/2b/159/994|/in/markchang|/in/aparnacd|/pub/garima-sinha/9/3b1/9b3|/in/nitinjulka|/in/jeannalee|/in/lcholmes|/in/elizabethburstein|/in/gustafalstromer|/in/jeffreysharris|/in/chelseytanaka|/pub/peter-birch/1/b59/568|/in/dshapero|/in/zalzally|/in/nicoposner|/in/kumareshp|/in/sarahtavel|/in/catherineshyu|/in/dforrester|/pub/mat-mullen/7/353/a02|/in/srothmuller|/in/srikanthmnaidu|/in/jocelynmangan|/in/dvdkm|/in/sanazahari|/pub/brandon-savage/3/889/231|/in/productmanagercalifornia|/in/jasontoff|/in/kartikrao|/pub/madhu-prabaker/0/3b6/9bb|/in/johnloof|/in/dweekly|/in/jppark|/in/andriheidar|/in/curtjohnson|/in/danielayele|/in/lizreaveswalker|/in/merci|/pub/paul-karayan/7/952/601|/in/maxkirsch|/in/salgar|/in/jchang1|/pub/matt-schnitt/55/753/941|/in/reevethompson|/in/jgolden|/in/maxhaltman|/in/roddynegron|/in/lennyrachitsky|/in/michaelboeke|/in/stephanieteng|/in/djhalliday|/in/deepthim|/pub/leili-baghaei-rad/15/735/379|/in/stephenchau|/in/stevenjlee|/in/ragavansrinivasan|/in/andrewjchang1|/in/iylee|/in/iamdeepa|/in/vidyachandra|/in/calvinclee|/in/stedjamulia')), callback='parse_person', follow=False)]
+	start_urls = ['https://www.linkedin.com/in/triketora', 'https://www.linkedin.com/pub/haile-owusu/54/5a0/b74', 'https://www.linkedin.com/in/lcholmes']
+  	rules = [Rule(SgmlLinkExtractor(allow=(r'www\.linkedin\.com/in/|www\.linkedin\.com/pub/')), callback='parse_person', follow=True)]
 	urlCounter = 0
 
 	def parse_person(self, response):
@@ -113,7 +44,7 @@ class PersonScraper(CrawlSpider):
 		person['current_position_start_date'] = response.xpath("//div[@class='editable-item section-item current-position']/div/span[@class='experience-date-locale']/time/text()").extract()
 		if person['current_position_start_date'] == []:
 			person['current_position_start_date'] = [""]
-		person['current_position_start_date'][0] = int(re.search(r'\d+', person['current_position_start_date'][0]).group())
+		person['current_position_start_date'][0] = person['current_position_start_date'][0]
 
 
 		#URL
@@ -216,7 +147,7 @@ class PersonScraper(CrawlSpider):
 
 			person['educationList'].append(tempEducation)
 
-		sleep(randint(1,3))
+		sleep(randint(1,4))
 
 		PersonScraper.urlCounter+=1
 		return person
